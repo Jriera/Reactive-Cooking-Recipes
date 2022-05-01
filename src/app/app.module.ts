02a35a89505201b81605f3f20181b3b7bb6de5a9
Jordi Riera <jordiriera81@gmail.com>
2022-05-01 18:11:07 +0200

    chore:add imports and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import {DataViewModule} from "primeng/dataview";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RecipesListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        DataViewModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
