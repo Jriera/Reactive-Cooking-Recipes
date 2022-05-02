7ecca474d3c9ef949ea56aab374feeec3e78416f
Jordi Riera <jordiriera81@gmail.com>
2022-05-02 22:42:13 +0200

    refactor: Use recipes as Observable with value from service observable

--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -1,36 +1,24 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../core/model/recipe.model";
 import {RecipesService} from "../core/services/recipes.service";
-import {Subject, takeUntil} from "rxjs";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss']
 })
-export class RecipesListComponent implements OnInit,OnDestroy {
-  recipes!:Recipe[];
-  destroy$:Subject<void> = new Subject<void>();
-  /* REQUIRES UNSUBSCRIBE subscription: Subscription | undefined;*/
+export class RecipesListComponent implements OnInit{
+  recipes!:Observable<Recipe[]>;
+
+
   constructor(private service:RecipesService ) { }
 
   ngOnInit(): void {
-    /*IT WORKS BUT DEMANDS AN UNSUBSCRIBING DECLARATIVE METHOD
-    this.subscription=this.service.getRecipes().subscribe(recipes =>{
-      this.recipes=recipes;
-    });*/
+    this.recipes=this.service.recipes$
 
-    this.service.getRecipes().pipe(
-      takeUntil(this.destroy$)
-    ).subscribe(recipes =>{
-      this.recipes=recipes
-    })
   }
 
-  ngOnDestroy() {
-    /* REQUIRES UNSUBSCRIBE this.subscription?.unsubscribe();*/
-    this.destroy$.next();
-    this.destroy$.complete()
-  }
+
 
 }
