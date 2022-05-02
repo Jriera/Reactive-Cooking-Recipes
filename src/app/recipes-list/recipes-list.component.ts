88a468115f7a4fbd7dba4f2a9098da0a4744baec
Jordi Riera <jordiriera81@gmail.com>
2022-05-02 17:54:40 +0200

    fix:Change RxJS subscriptions to use operators to unsubscribe observable

--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from "../core/model/recipe.model";
 import {RecipesService} from "../core/services/recipes.service";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss']
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent implements OnInit,OnDestroy {
   recipes!:Recipe[];
+  destroy$:Subject<void> = new Subject<void>();
+  /* REQUIRES UNSUBSCRIBE subscription: Subscription | undefined;*/
   constructor(private service:RecipesService ) { }
 
   ngOnInit(): void {
-    this.service.getRecipes().subscribe(recipes =>{
+    /*IT WORKS BUT DEMANDS AN UNSUBSCRIBING DECLARATIVE METHOD
+    this.subscription=this.service.getRecipes().subscribe(recipes =>{
       this.recipes=recipes;
-    });
+    });*/
+
+    this.service.getRecipes().pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(recipes =>{
+      this.recipes=recipes
+    })
+  }
+
+  ngOnDestroy() {
+    /* REQUIRES UNSUBSCRIBE this.subscription?.unsubscribe();*/
+    this.destroy$.next();
+    this.destroy$.complete()
   }
 
 }
