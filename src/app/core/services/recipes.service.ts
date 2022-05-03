c743fcaa760a76a9917fe2b4ff04aebccb898b9f
Jordi Riera <jordiriera81@gmail.com>
2022-05-03 14:47:57 +0200

    feat:Implement error catching on http request

--- src/app/core/services/recipes.service.ts
+++ src/app/core/services/recipes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {catchError, Observable, of} from "rxjs";
 import {Recipe} from "../model/recipe.model";
 import {environment} from "../../../environments/environment";
 
@@ -9,7 +9,9 @@ import {environment} from "../../../environments/environment";
 })
 export class RecipesService {
   BASE_PATH=environment.basePath;
-  recipes$:Observable<Recipe[]> = this.http.get<Recipe[]>(`${this.BASE_PATH}/recipes`)
+  recipes$:Observable<Recipe[]> = this.http.get<Recipe[]>(`${this.BASE_PATH}/recipes`).pipe(
+    catchError(error=>of([]))
+  )
   constructor(private http:HttpClient) { }
 
 
